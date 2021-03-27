dc9250fcd3fe02bd014a13ab2ff4ccfe372b4d5b
GeFuchun <terrell0.32@gmail.com>
2021-03-27 17:08:28 +0800

    updated next_server.py

--- next_server.py
+++ next_server.py
@@ -1,5 +1,6 @@
 from console_IO import *
 from cloud_foundation import *
+import random
 
 def get_server_type(vm: 'VM', server_type_list: List['ServerType'], w_elec=0) -> 'ServerType':
     core = vm.node_core
@@ -7,6 +8,7 @@ def get_server_type(vm: 'VM', server_type_list: List['ServerType'], w_elec=0) ->
     ratio = core/memory
 
     capacity_list = [i for i, server in enumerate(server_type_list) if server.core_lmt > core and server.mem_lmt > memory] 
-    scores = [(server_type_list[i].core/server_type_list[i].memory - ratio)**2 *(server_type_list[i].price+server_type_list[i].cost_each_day*w_elec) for i in capacity_list]
+    # scores = [(server_type_list[i].core/server_type_list[i].memory - ratio)**2 *(server_type_list[i].price+server_type_list[i].cost_each_day*w_elec) for i in capacity_list]
+    scores = [((server_type_list[i].core/server_type_list[i].memory - ratio)**2 *(server_type_list[i].price+server_type_list[i].cost_each_day*w_elec)/(server_type_list[i].core+(0.5+random.random())*server_type_list[i].memory)**(w_elec/16))*random.random() for i in capacity_list]
     idx=capacity_list[scores.index(min(scores))]
     return server_type_list[idx]
